0df51c7d9b29b93ebb02cdc0189d0400a80cd829
tricialeach <tleach@gmail.com>
2017-11-12 13:37:29 -0800

    Add homepage.png files for all fall students

--- students/fall17/grad-tiles.php
+++ students/fall17/grad-tiles.php
@@ -3,7 +3,7 @@
     <!--START PHOTO GRID-->
     <ul class="photoGrid">
         <?php
-            $files = glob('students/sm17/*', GLOB_ONLYDIR);
+            $files = glob('students/fall17/*', GLOB_ONLYDIR);
 
             foreach($files as $dir) {
                 $dirname = basename($dir);
@@ -12,16 +12,13 @@
                 $firstName = ucwords($nameArray[0]);
                 $lastName = ucwords($nameArray[1]);
                 $thirdName = ' ' . ucwords($nameArray[2]);
-                $photofile = 'students/sm17/'.$dirname.'/portraits/homepage.jpg';
+                $photofile = 'students/fall17/'.$dirname.'/portraits/homepage.png';
 
                 echo '<li class="studentBlock">
                     <img class="photo" src="'. $photofile .'" alt="'. 'A photo of '.$nameUppercase .'" />
                     <a href="grad.php?name='. $dirname .'">
                         <div>
-                            <svg class="sm17--block-svg" width="480" height="480" viewBox="0 0 480 480" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M3 3h474v474H3z" stroke="#000" stroke-width="4" fill="none"/>
-                            </svg>
-                            <h3>'. '<span class="sm17--firstname">'. $firstName .'</span>'.'<span class="sm17--lastname">'. $lastName . $thirdName .'</span>' .'</h3>
+                            <h3>'. '<span class="fall17--studentname">'. $firstName . ' '. $lastName . $thirdName .'</span>' .'</h3>
                         </div>
                     </a>
                 </li>';
