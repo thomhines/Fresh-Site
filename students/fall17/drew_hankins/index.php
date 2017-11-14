7151648aadade680dfae91471c313cbc9dda5469
tricialeach <tleach@gmail.com>
2017-11-14 00:37:44 -0800

    Updated portraits for students

--- students/fall17/drew_hankins/index.php
+++ students/fall17/drew_hankins/index.php
@@ -8,7 +8,7 @@
 </div>
 
 <div class="c6 bio">
-    <h1 style="text-transform:uppercase;">Aaron <span class="last-name">Osborn</span></h1>
+    <h1 style="text-transform:uppercase;">Drew <span class="last-name">Hankins</span></h1>
 
     <p>I’m an illustrator and designer from the Northwest, with a humble background in editorial illustration, production, marketing, playing music, total shredding, and walks at night. I want to work on magic and humanizing stories with good people. I like to ramble about the gravitational influence of the moon on earth, and my favorite X-files episodes, but I won’t do that here.</p>
     <img class="artwork" src="students/sp17/aaron_osborn/work_examples/caseapp_aaron_osborn.png" alt="Case App">
