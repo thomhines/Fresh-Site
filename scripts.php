67ccdf9881601fb824f7ad027537d4ac1ce19191
tricialeach <tleach@gmail.com>
2017-11-20 22:27:11 -0800

    Commented out some links to js files

--- scripts.php
+++ scripts.php
@@ -1,6 +1,6 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="src/js/script.js"></script>
-<script src="src/js/jquery.easing.min.js"></script>
+<!-- <script src="src/js/jquery.easing.min.js"></script> -->
 <script src="src/js/classie.js"></script>
-<script type="text/javascript" src="src/js/jquery.flexslider-min.js"></script>
-<script src="src/js/app.js"></script>
+<!-- <script type="text/javascript" src="src/js/jquery.flexslider-min.js"></script> -->
+<!-- <script src="src/js/app.js"></script> -->
