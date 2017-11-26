cd1a96c463ffeef705a034e0eb899f4563aed803
tricialeach <tleach@gmail.com>
2017-11-26 02:08:06 -0800

    Updated files for Rayan Alhu

--- students/fall17/rayan_alhu/index.php
+++ students/fall17/rayan_alhu/index.php
@@ -1,5 +1,5 @@
 <div class="s2 c2 bioleft">
-<div class="bio_pic" style="background-image: url(students/fall17/rayan_alhú/portraits/rayan_alhú_02.jpg); background-color: white;"><img src="students/fall17/rayan_alhú/portraits/rayan_alhu_01.jpg" alt="Rayan Alhú" onmouseover="this.src='students/fall17/rayan_alhú/portraits/rayan_alhu_02.jpg';" onmouseout="this.src='students/fall17/rayan_alhú/portraits/rayan_alhu_01.jpg';"></div>
+<div class="bio_pic" style="background-image: url(students/fall17/rayan_alhu/portraits/rayan_alhu_02.jpg); background-color: white;"><img src="students/fall17/rayan_alhu/portraits/rayan_alhu_01.jpg" alt="Rayan Alhú" onmouseover="this.src='students/fall17/rayan_alhu/portraits/rayan_alhu_02.jpg';" onmouseout="this.src='students/fall17/rayan_alhu/portraits/rayan_alhu_01.jpg';"></div>
 
 
     <div class="website_btn">
@@ -17,16 +17,16 @@
     <h4 class="bio_question">
 If you were a plant, what would you be?</h4>
     <p>If Rayan was a plant he would be a cactus, because he is patient, quiet, and full of inner surprises. </p>
-    <img class="artwork" src="students/fall17/rayan_alhú/ralhu_image1.jpg" alt="Rayan Doll">
+    <img class="artwork" src="students/fall17/rayan_alhu/ralhu_image1.jpg" alt="Rayan Doll">
     <h2>Rayan Doll</h2>
     <p>Rayan Doll was my senior thesis project which is a website that displays my personal story at different stages of life. A psychological battle between myself and society results in the manufacturing of the brainwashed Rayan Doll while being packaged in different boxes. </p>
-    <img class="artwork" src="students/fall17/rayan_alhú/ralhu_image2.jpg" alt="Olé">
+    <img class="artwork" src="students/fall17/rayan_alhu/ralhu_image2.jpg" alt="Olé">
     <h2>Olé</h2>
-    <p>My admiration for Gypsy Flamenco music started when I attended a Flamenco performance in Mardid few years ago. The fire of passion of vocalists and dancers I saw and felt inspired me to design the first Flamenco café & vino in Portland, Oregon—the city that admires art and culture.</p>
-    <img class="artwork" src="students/fall17/rayan_alhú/ralhu_image3.jpg" alt="Opera">
+    <p>My admiration for Gypsy Flamenco music started when I attended a Flamenco performance in Mardid few years ago. The fire of passion of vocalists and dancers I saw and felt inspired me to design the first Flamenco café &amp; vino in Portland, Oregon—the city that admires art and culture.</p>
+    <img class="artwork" src="students/fall17/rayan_alhu/ralhu_image3.jpg" alt="Opera">
     <h2>Opera</h2>
     <p>Opera is a condominium building designed by the late Iraqi-British architect Dame Zaha Hadid. The dramatic exteriors of the building reminded me of an opera performance, that I enjoyed re-branding it.  </p>
-    <img class="artwork" src="students/fall17/rayan_alhú/ralhu_image4.jpg" alt="Bambino ">
+    <img class="artwork" src="students/fall17/rayan_alhu/ralhu_image4.jpg" alt="Bambino ">
     <h2>Bambino </h2>
     <p>Bambino is a nutritional beverage and lifestyle brand for kids on the go. It is aimed for all American kids of different racial backgrounds and all seasons of the year shown through its playful illustrations on milk packaging.</p>
 </div>
