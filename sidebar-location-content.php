3dbf6c7461eb7b0674d990aa77123c6fc4d01478
thomhines <thomhines@gmail.com>
2018-03-12 23:49:28 -0700

    Updated Kamp Grizzly address

--- sidebar-location-content.php
+++ sidebar-location-content.php
@@ -3,9 +3,13 @@
     <img class="revlogo" src="src/img/winter2018/kamp-grizzly-photoshoot2.jpg" alt="photo of location" width="100%" />
   </a>
 </div>
-    
+
 <div class="eventData clear">
-  <p>This year, FRESH is at <a target="_blank" href="http://kampgrizzly.com">Kamp Grizzly</a> on Tuesday, March 20th from 5:30&ndash;8:30 pm. The address is 2316 NE Oregon St <a target="_blank" href="https://www.google.com/maps/place/2316+NE+Oregon+St+%23109,+Portland,+OR+97232/@45.5282009,-122.6441381,17z/data=!3m1!4b1!4m5!3m4!1s0x5495a0b81e1970ef:0xce6fd691d1b9b66c!8m2!3d45.5282009!4d-122.6419494">(map)</a>.</p>
+  <p>This year, FRESH is at <a target="_blank" href="http://kampgrizzly.com">Kamp Grizzly</a> on Tuesday, March 20th from 5:30&ndash;8:30 pm. The address is<br>
+Kamp Grizzly Photo Studio<br>
+2229 Northeast Irving Street<br>
+FOLLOW THE BALLOONS<br>
+Portland, OR 97232 <a target="_blank" href="https://www.google.com/maps/place/2229+NE+Irving+St,+Portland,+OR+97232/@45.5282381,-122.6453198,17z/data=!3m1!4b1!4m5!3m4!1s0x5495a0b827c76931:0xb2e4d1d9d9979312!8m2!3d45.5282381!4d-122.6431311">(map)</a>.</p>
   <p>If you would like to speak to or collaborate with our PSUGD grads, please register through <a target="_blank" href="https://psugd2018winterfresh.eventbrite.com/">Eventbrite</a> to attend Fresh.</p>
   <p>A special thank you to <a target="_blank" href="http://kampgrizzly.com">Kamp Grizzly</a> for their generous donation of the event space. &hearts;</p>
 </div>
