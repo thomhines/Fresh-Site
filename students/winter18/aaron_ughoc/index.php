599457c794e27deaa3514bd9f84a51bc1ca04c05
thomhines <thomhines@gmail.com>
2018-03-13 14:03:51 -0700

    Fixed grid layout issue (inconsistent image height measurements), updated images and typos in student profiles

--- students/winter18/aaron_ughoc/index.php
+++ students/winter18/aaron_ughoc/index.php
@@ -12,29 +12,29 @@
     <h1>Aaron <span class="last-name">Ughoc</span></h1>
     <ul class="interests"><li>Editorial Design</li><li>Photo Collage</li><li>Illustration</li><li>Branding</li><li>Art Direction</li></ul>
 
-    <p>Aaron Ughoc is a real human being that lives in SE Portland and has a cat named Steve French. His work is informed by counter culture, cartoons, bad jokes, and everyday life. Aaron aims to create graphics that approach subject matter from an unobvious and obvious perspective in a effort to find a middle ground where the magic of deign happens. He likes to draw in his free time, watch Friends, play ukulele, and hack a sack with my roommates. He’s also from California proud of it.</p>
-    
+    <p>Aaron Ughoc is a real human being that lives in SE Portland and has a cat named Steve French. His work is informed by counter culture, cartoons, bad jokes, and everyday life. Aaron aims to create graphics that approach subject matter from an unobvious and obvious perspective in order to find a middle ground which produces engaging design. He likes to draw in his free time, watch Friends, play ukulele, and hack a sack with his roommates. He’s also from California and proud of it.</p>
+
     <!--
     <h4 class="bio_question">What is your favorite plant?</h4>
     <p>Anything edible. My parakeets love basil.</p>
     -->
-    
+
     <img class="artwork" src="students/winter18/aaron_ughoc/a_ughoc_image1.jpg" alt="Photo of Project 1">
     <h2>PSU Vanguard</h2>
     <p>Starting the winter term of 2018, I became the creative directer of the Portland State Vanguard, a weekly student run newspaper. My main roles at the Vanguard were to manage a team of designers, create weekly covers/center spreads to accompany news stories, design special guides, art direct photo shoots, and curate a weekly comics page. Working for the VG has been one of the most rewarding opportunities I’ve had the honor of accepting. The VG has taught me how to manage time, people, stress, and most of all, how to grab a readers attention. I value my time spent in this role and cherish the friendships and work I’ve made through the Vanguard.</p>
-    
+
     <img class="artwork" src="students/winter18/aaron_ughoc/a_ughoc_image2.jpg" alt="Photo of Project 2">
     <h2>Two Things</h2>
     <p>Two things is a personal project I started last summer as a creative exercise when I had nothing going on. The idea of the project is to take two completely unrelated images that are buried deep in royalty free archives and combine them in digital photo collage in order to create something new and interesting. Most great design combines two things to solve a visual problem and this project has become a warm up exercise to get those creative juices flowing.</p>
-    
+
     <img class="artwork" src="students/winter18/aaron_ughoc/a_ughoc_image3.jpg" alt="Photo of Project 3">
     <h2>DMV Rebrand</h2>
     <p>The goal of this rebrand it to take back the DMV for the people. For too long its been a drag to go to the DMV and theres no reason why it should be a horrible place to go. With the help of a rebrand we can get people excited about the DMV and happy to go get their license renewed. Lets take back the Oregon DMV for the people.</p>
-    
+
     <img class="artwork" src="students/winter18/aaron_ughoc/a_ughoc_image4.jpg" alt="Photo of Project 4">
     <h2>A&O Connect</h2>
     <p>Last year two students started the Athletic and Outdoor Connection at Portland State University with the hopes of connecting students to the A&O industry and bridging connections to industry leaders. Once a month they invite industry professionals to speak to students about their respective careers. This rebrand aims to visually content the outdoors with student life and the forward thinking spirit of the group in an effort to increase membership and brand awareness.</p>
-    
+
 </div>
 
 <!--FOOTER -->
