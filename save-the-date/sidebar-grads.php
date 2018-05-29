9397bf23da1250e9852d121a1af6829314847ea6
kcharlson <32726209+kcharlson@users.noreply.github.com>
2018-05-29 14:53:36 -0700

    initial commit

---
+++ save-the-date/sidebar-grads.php
@@ -0,0 +1,10 @@
+<nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-right" id="cbp-spmenu-s2">
+	<div class="flyoutbtn gradbtn">
+		<button id="showRight">FRESH?</button>
+	</div>
+	<div class="profilescroll">
+
+  <?php include 'sidebar-grads-content.php'; ?>
+    
+	</div>
+</nav>
---
+++ save-the-date/sidebar-location.php
@@ -0,0 +1,10 @@
+<nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="cbp-spmenu-s1">
+	<div class="flyoutbtn infobtn">
+		<button id="showLeft">EVENT INFO</button>
+	</div>
+	<div class="eventinfo profilescroll">
+
+    <?php include 'sidebar-location-content.php'; ?>
+
+	</div>
+</nav>
