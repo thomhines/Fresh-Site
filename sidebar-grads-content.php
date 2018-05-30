3d1d8f046ef793a3f9d0300bc711f62b79a74174
kcharlson <32726209+kcharlson@users.noreply.github.com>
2018-05-30 16:10:58 -0700

    Add additional content grad tab/Cleaned up CSS
    Added new image, and sponsor logos to the right hand "grad/Fresh?" tab.
    
    Included links with sponsor logos
    
    Cleaned up CSS with beautify, and added specific styles to match needs of the new layout

--- sidebar-grads-content.php
+++ sidebar-grads-content.php
@@ -1,7 +1,17 @@
 <div class="eventSpace">
-  <img class="revlogo" src="src/img/spring2018/05-april.jpg" alt="photo of location" width="100%"/>
+  <img class="revlogo" src="src/img/spring2018/DSC_0452.jpg" alt="photo of location" width="100%"/>
 </div>
-
+    
 <div class="previous_grads">
   <p>FRESH is an invitation-only review held at the end of every <a target="_blank" href="http://psu.gd">PSUGD</a> Senior Portfolio class. FRESH typically happens three times a year (December, June and the end of August). The event is organized by the graduating students. It not only showcases their work, but gives them an opportunity to receive valuable feedback from respective design leaders, and make connections with the design community.</p>
+</div>
+
+
+<div class="sponsors">
+    <h2>Thanks to our Sponsors!</h2>
+    <div class="logos">
+        <a href="https://www.halfcourtstudio.com/"><img id="halfcourt" src="src/img/spring2018/halfcourt_logo-01.svg"></a>
+        <a href="http://www.customblocksportland.com/"><img src="src/img/spring2018/CapstonePartnersLogo_2015_Bluegrey.svg"></a>
+        <a href="https://www.sizzlepie.com/"><img src="src/img/spring2018/sizzle_pie_logo.svg"></a>
+    </div>
 </div>
