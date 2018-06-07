c08216ced747b9993c60234b55e917add7ab3af8
kcharlson <32726209+kcharlson@users.noreply.github.com>
2018-06-07 13:25:28 -0700

    Add Lauren Klinkhammer
    Adding Lauren Klinkhammer's profile to the student folder section

---
+++ students/spring18/lauren_klinkhammer/index.php
@@ -0,0 +1,48 @@
+<div class="s2 c2 bioleft">
+<div class="bio_pic" ><img src="students/spring18/lauren_klinkhammer/Laken1.jpg" alt="Photo of Student"></div>
+
+
+    <div class="website_btn">
+        <a target="_blank" href="https://www.lknwrght.com/">VIEW WEBSITE</a>
+    </div>
+</div>
+
+<div class="c6 bio">
+
+    <h1>Lauren<span class="last-name"> Klinkhammer</span></h1>
+    <ul class="interests">
+    <li>Branding and Identity</li>
+    <li>Illustration</li>
+    <li>Packaging</li>
+    <li>Energetic Presentation Giving</li>
+    </ul>
+
+    <p>Laken creates digital and print experiences that promote experimentation and playfulness. She filters illustrative and tactile art through a designer's lens to create meaningful solutions for an increasingly online world. She is excited to make collaborative work with good people who have new podcast recommendations and can appreciate her cherished rock collection.</p>
+
+    <!--
+    <h4 class="bio_question">What is your favorite plant?</h4>
+    <p>Anything edible. My parakeets love basil.</p>
+    -->
+
+    <img class="artwork" src="students/spring18/lauren_klinkhammer/l_klinkhammer_image1.jpg" alt="Photo of Project 1">
+    <h2>Wolpertinger</h2>
+    <p>Wolpertinger is a branding and packaging design project. I created Wolpertinger as a fresh take on German beer identities. While the aim is to be more fun and less traditional, the brand doesn't lose touch with Bavarian storytelling and symbolism.</p>
+
+    <img class="artwork" src="students/spring18/lauren_klinkhammer/l_klinkhammer_image2.jpg" alt="Photo of Project 2">
+    <h2>Run a Show</h2>
+    <p>An informational riso print showing new sound engineers the ins-and-outs of running a show at this local punk venue.</p>
+
+    <img class="artwork" src="students/spring18/lauren_klinkhammer/l_klinkhammer_image3.jpg" alt="Photo of Project 3">
+    <h2>Purlgrrrl</h2>
+    <p>Purlgrrrl is a brand and packaging identity for my knitting-enthusiast friend, Fiona. A heavily tattooed woman who loves punk rock, she needed a new identity for her small business that spoke true to herself and stood out in the mosh pit. </p>
+
+    <img class="artwork" src="students/spring18/lauren_klinkhammer/l_klinkhammer_image4.jpg" alt="Photo of Project 4">
+    <h2>Portland Mercury cover</h2>
+    <p>This Mercury cover was a practice in illustration and page design. I wanted to illustrate Portland's Burger Week as a large inviting spectacle. I was inspired by large public art installations that involve the whole community. </p></p> 
+    
+
+</div>
+
+<!--FOOTER -->
+<?php include 'footer.php'; ?>
+<!--end FOOTER-->
